cc7f4247e3da23731c233ec9a5dd4ce0307ce28d
kenny <saki514103@gmail.com>
2015-10-25 15:28:52 +0800

    first commit

---
+++ BSP.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+struct TreeNode** generateTrees(int n, int* returnSize) {
+    
+}
